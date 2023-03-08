f383b88a9eb95406906cddd57d3129fea5ef2534
Khuram Zaman <khuram.zaman1@gmail.com>
2023-03-08 15:56:15 -0500

    Formatted multi-line arguments properly
    I realized reading the for loop and pandas dataframe was hard to read so I formatted it by placing each argument parameter in a unique line.

--- scrape_unlimited_tweets_by_hashtag.py
+++ scrape_unlimited_tweets_by_hashtag.py
@@ -4,7 +4,6 @@ Install Packages
 # pip install pandas
 '''
 
-
 import csv
 from datetime import date
 import pandas as pd
@@ -13,14 +12,14 @@ import time
 
 
 
-hashtag_query = "hashtag"
+hashtag_query = "islam"
 
 # Set the limit fow how many tweets you want to correct and convert it into a string in a separate variable to use it in the filename
-limit = 1000000
+limit = 10
 limit_as_string = str(limit)
 
 # Create the filename
-filename = "your_file_directory\_" + limit_as_string + "_tweets_" + hashtag_query + "_" + str(date.today().strftime("%m-%d-%y")) + ".csv"
+filename = "your_file_path\_" + limit_as_string + "_tweets_" + hashtag_query + "_" + str(date.today().strftime("%m-%d-%y")) + ".csv"
 print("Filename Generated: ", filename)
 
 # Create a list to store the tweets in
@@ -45,12 +44,72 @@ for tweet in sntwitter.TwitterSearchScraper(hashtag_query).get_items():
         if count % 2000 == 0:
             print("Going to sleep for ", sleep_time/60, " minutes")
             time.sleep(sleep_time)
-        tweets.append([tweet.date, tweet.url, tweet.content, tweet.renderedContent, tweet.id, tweet.user.username, tweet.user.displayname, tweet.user.id, twitter_profile_url + tweet.user.username, tweet.user.description, tweet.user.verified, tweet.user.created, tweet.user.followersCount, tweet.user.friendsCount, tweet.user.statusesCount, tweet.user.location, tweet.user.descriptionUrls, tweet.user.linkUrl, tweet.user.profileImageUrl, tweet.user.profileBannerUrl, tweet.replyCount, tweet.retweetCount, tweet.likeCount, tweet.quoteCount, tweet.lang, tweet.source, tweet.retweetedTweet, tweet.quotedTweet, tweet.mentionedUsers, tweet.hashtags])
-
-
+        tweets.append([
+            tweet.date,
+            tweet.url,
+            tweet.content,
+            tweet.renderedContent,
+            tweet.id,
+            tweet.user.username,
+            tweet.user.displayname,
+            tweet.user.id,
+            twitter_profile_url + tweet.user.username,
+            tweet.user.description,
+            tweet.user.verified,
+            tweet.user.created,
+            tweet.user.followersCount,
+            tweet.user.friendsCount,
+            tweet.user.statusesCount,
+            tweet.user.location,
+            tweet.user.descriptionUrls,
+            tweet.user.linkUrl,
+            tweet.user.profileImageUrl,
+            tweet.user.profileBannerUrl,
+            tweet.replyCount,
+            tweet.retweetCount,
+            tweet.likeCount,
+            tweet.quoteCount,
+            tweet.lang,
+            tweet.source,
+            tweet.retweetedTweet,
+            tweet.quotedTweet,
+            tweet.mentionedUsers,
+            tweet.hashtags
+        ])
         print("Scraped Tweets:", count)
 
-df = pd.DataFrame(tweets, columns=['date', 'tweet_url', 'tweet_content', 'tweet_rendered_content','tweet_id', 'user_name', 'display_name', 'user_id', 'concatenated_profile_url', 'user_description', 'verified', 'user_profile_created', 'user_follower_count', 'user_friend_count', 'user_statuses_count', 'user_location', 'user_description_url', 'user_profile_url', 'user_profile_image_url', 'user_profile_banner_url', 'tweet_reply_count', 'tweet_retweet_count', 'tweet_like_count', 'tweet_quote_count', 'tweet_language', 'tweet_source', 'rt_original_tweet_id', 'quoted_tweet_original_tweet_id', 'tweet_mentioned_users', 'tweet_hashtags'])
+df = pd.DataFrame(tweets, columns=[
+    'date',
+    'tweet_url',
+    'tweet_content',
+    'tweet_rendered_content',
+    'tweet_id',
+    'user_name',
+    'display_name',
+    'user_id',
+    'concatenated_profile_url',
+    'user_description',
+    'verified',
+    'user_profile_created',
+    'user_follower_count',
+    'user_friend_count',
+    'user_statuses_count',
+    'user_location',
+    'user_description_url',
+    'user_profile_url',
+    'user_profile_image_url',
+    'user_profile_banner_url',
+    'tweet_reply_count',
+    'tweet_retweet_count',
+    'tweet_like_count',
+    'tweet_quote_count',
+    'tweet_language',
+    'tweet_source',
+    'rt_original_tweet_id',
+    'quoted_tweet_original_tweet_id',
+    'tweet_mentioned_users',
+    'tweet_hashtags'
+])
 print(df)
 
 df.to_csv(filename)
